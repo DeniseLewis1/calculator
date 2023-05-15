c98c17d6b646a8543a53b973605a77f516bed0f7
Denise <deniselewis1120@gmail.com>
2023-05-15 09:03:05 -0700

    Added functionality

--- script.js
+++ script.js
@@ -1,3 +1,49 @@
 const buttons = document.querySelectorAll("button");
 const display = document.querySelector(".display");
 
+let evalStr = [];
+let numStr = [];
+let hasDecimal = false;
+
+function buttonClicked () {  
+    if (this.classList.contains("clear-data")) {
+        display.innerText = 0;
+        resetData();
+
+    } else if (this.classList.contains("equal")) {
+        evalStr.push(...numStr);
+        display.innerText = eval(evalStr.join(""));
+        resetData();
+
+    } else if (this.classList.contains("operator")) {
+        if (numStr.length !== 0) {
+            evalStr.push(...numStr);
+            numStr = [];
+            hasDecimal = false;
+        }        
+
+        if (evalStr[evalStr.length - 1] === "/" || evalStr[evalStr.length - 1] === "*" || evalStr[evalStr.length - 1] === "-" || evalStr[evalStr.length - 1] === "+") {
+            evalStr.pop();
+        }
+
+        evalStr.push(this.textContent);
+
+    } else if (this.classList.contains("num") || !hasDecimal) {
+        if (this.classList.contains("decimal")) {
+            hasDecimal = true;
+        }
+
+        numStr.push(this.textContent);
+        display.innerText = numStr.join("");
+        
+    }
+}
+
+function resetData () {
+    evalStr = [];        
+    numStr = [];
+    hasDecimal = false;
+}
+
+buttons.forEach(button => button.addEventListener("click", buttonClicked));
+
