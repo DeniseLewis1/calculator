b3a0dbc6e5b8815b88e80625ebd7354810be1ce4
Denise <deniselewis1120@gmail.com>
2023-06-23 14:33:17 -0700

    Accept keyboard input

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let evalStr = [];
 let numStr = [];
 let hasDecimal = false;
 
-function buttonClicked () {  
+function buttonClicked () {
     if (this.classList.contains("clear-data")) {
         display.innerText = 0;
         resetData();
@@ -28,14 +28,50 @@ function buttonClicked () {
 
         evalStr.push(this.textContent);
 
-    } else if (this.classList.contains("num") || !hasDecimal) {
-        if (this.classList.contains("decimal")) {
-            hasDecimal = true;
-        }
-
+    } else if (this.classList.contains("num")) {
         numStr.push(this.textContent);
         display.innerText = numStr.join("");
         
+    } else if (this.classList.contains("decimal") && !hasDecimal) {
+        numStr.push(this.textContent);
+        display.innerText = numStr.join("");
+        hasDecimal = true;
+    }
+}
+
+function keyboardInput (input) {
+    const numInput = /[0-9]/;
+
+    if (input.toLowerCase() === "c" || input === "Clear") {
+        display.innerText = 0;
+        resetData();
+
+    } else if (input === "Enter" || input === "=") {
+        evalStr.push(...numStr);
+        display.innerText = eval(evalStr.join(""));
+        resetData();
+
+    } else if (input === "/" || input === "*" || input === "-" || input === "+") {
+        if (numStr.length !== 0) {
+            evalStr.push(...numStr);
+            numStr = [];
+            hasDecimal = false;
+        }        
+
+        if (evalStr[evalStr.length - 1] === "/" || evalStr[evalStr.length - 1] === "*" || evalStr[evalStr.length - 1] === "-" || evalStr[evalStr.length - 1] === "+") {
+            evalStr.pop();
+        }
+
+        evalStr.push(input);
+
+    } else if (input.match(numInput)) {
+        numStr.push(input);
+        display.innerText = numStr.join("");
+
+    } else if (input === "." && !hasDecimal) {
+        numStr.push(input);
+        display.innerText = numStr.join("");
+        hasDecimal = true;
     }
 }
 
@@ -47,3 +83,6 @@ function resetData () {
 
 buttons.forEach(button => button.addEventListener("click", buttonClicked));
 
+document.addEventListener("keydown", (e) => {
+    keyboardInput(e.key);
+});
